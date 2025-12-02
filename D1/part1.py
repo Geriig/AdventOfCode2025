1b1066131d64308c0718977e9b1b86280ee88715
Kiss Gergely <diak_262.kiss.gergely@11.d.2026.com>
2025-12-02 09:22:39 +0100

    D1P1py

---
+++ D1/part1.py
@@ -0,0 +1,27 @@
+#filename = "example.txt"
+filename = "input.txt"
+
+f = open(filename, "r")
+
+volume = 50
+zeroes = 0
+
+lines = f.readlines()
+for line in lines:
+    direction = line[0]
+    steps = int(line[1:])
+    for i in range(steps):
+        if direction == 'L':
+            volume -= 1
+            if  volume < 0:
+                volume = 99
+        else:
+            volume +=1
+            if volume > 99:
+                volume=0
+    if volume == 0:
+        zeroes +=1
+
+print(zeroes)
+
+    
